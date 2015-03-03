f3c2f88306c49d07f2a1bb603eff77d60bfccabe
Aleksey Pesternikov <ap@Alekseys-MacBook-Pro.local>
2015-03-03 05:15:02 -0800

    auth

--- src/backplane/auth.go
+++ src/backplane/auth.go
@@ -14,13 +14,17 @@ type basicAuthWrapper struct {
 
 func (b *basicAuthWrapper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	username, password, ok := r.BasicAuth()
-	if !ok || b.Config.Userpass[username] != password {
-		w.Header().Set("WWW-Authenticate", `Basic realm="`+b.Config.Realm+`"`)
-		w.WriteHeader(401)
-		w.Write([]byte("401 Unauthorized\n"))
-		return
+	if ok {
+		storedpass, ok := b.Config.Userpass[username]
+		if ok && storedpass == password {
+			b.Handler.ServeHTTP(w, r)
+			return
+		}
 	}
-	b.Handler.ServeHTTP(w, r)
+	w.Header().Set("WWW-Authenticate", `Basic realm="`+b.Config.Realm+`"`)
+	w.WriteHeader(401)
+	w.Write([]byte("401 Unauthorized\n"))
+	return
 }
 
 func AuthWrapper(cf *config.Auth, h http.Handler) (http.Handler, error) {
